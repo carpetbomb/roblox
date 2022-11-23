67d5c8b8cc331ca9acc3bcdd49bc852deceec594
carpetbomb <118927537+carpetbomb@users.noreply.github.com>
2022-11-23 18:35:07 +0000

    Create ACS_Vuln.lua

---
+++ ACS_Vuln.lua
@@ -0,0 +1,44 @@
+local found1 = false
+local found2 = false
+
+function toast(tit, con, col)
+    syn.toast_notification({
+        Type = ToastType.Info,
+        Duration = 7.5,
+        Title = "Found 1",
+        Content = "Found ACS Module 1!",
+        IconColor = Color3.fromRGB(col, 234, 102)
+    })
+end
+for i,v in pairs(game:GetDescendants()) do
+	if v.Name == "Recarregar" then
+		event = v
+	end
+	if v.Name == "Refil" then
+		event2 = v
+	end
+end
+
+if event then
+    toast("Found ACS1", "", 128)
+else
+    toast("No ACS1", "", 234)
+end
+
+if event2 then
+    toast("Found ACS2", "", 128)
+else
+    toast("No ACS2", "", 234)
+end
+
+if event then
+    getgenv().SetValue = function(ValueObj,NewValue) -- NewValue
+        event:FireServer(NewValue,{ACS_Modulo = {Variaveis = {StoredAmmo = ValueObj}}})
+    end
+end
+
+if event2 then
+    getgenv().SetValue2 = function(ValueObj,NewValue) -- NewValue
+        event2:FireServer(NewValue,{ACS_Modulo = {Variaveis = {StoredAmmo = ValueObj}}})
+    end
+end
